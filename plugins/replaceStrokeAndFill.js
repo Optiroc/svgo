c93582c440f678a15a688c760bc4c0019c3c97a3
David Lindecrantz <dlz@teenageengineering.com>
2014-12-16 16:27:00 +0100

    Added replaceStrokeAndFill plugin

---
+++ plugins/replaceStrokeAndFill.js
@@ -0,0 +1,49 @@
+'use strict';
+
+exports.type = 'perItem';
+
+exports.active = false;
+
+exports.params = {
+  stroke: true,
+  fill: true
+};
+
+var regStrokeProps = /^stroke/,
+regFillProps = /^fill/;
+
+/**
+* Replace stroke and fill attrs with classes "s" and "f".
+*
+* @param {Object} item current iteration item
+* @param {Object} params plugin params
+* @return {Boolean} if false, item will be filtered out
+*
+* @author dlz
+*/
+exports.fn = function(item, params) {
+  if (item.isElem()) {
+
+    // remove gradient elements
+    if (params.fill && item.isElem('linearGradient')) {
+      return false;
+    }
+
+    // replace fill
+    if (params.fill && !item.isElem('svg') && !item.hasAttr('fill', 'none')) {
+      item.eachAttr(function(attr) {
+        if (regFillProps.test(attr.name)) { item.removeAttr(attr.name); }
+      });
+      item.addClass('f');
+    }
+
+    // replace stroke
+    if (params.stroke && item.hasAttr('stroke')) {
+      item.eachAttr(function(attr) {
+        if (regStrokeProps.test(attr.name)) { item.removeAttr(attr.name); }
+      });
+      item.addClass('s');
+    }
+
+  }
+};
